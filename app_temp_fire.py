99b735db767f882fe94281ef3c57180121d7ca41
wrwr4859 <wenlirui@gmail.com>
2024-08-25 19:46:50 -0700

    update fire temp app

--- app_temp_fire.py
+++ app_temp_fire.py
@@ -77,4 +77,4 @@ def index():
     
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5002)
